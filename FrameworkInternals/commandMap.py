67a9c8a2fedd5b6155cce08387fcf2d7e202d688
Damian Abalo Miron <wardamo.zero@gmail.com>
2016-05-24 11:20:28 +0200

    Forgot to add the file commandMap.py in my last commit, fixing that.

---
+++ FrameworkInternals/commandMap.py
@@ -0,0 +1,21 @@
+
+commandMap = {
+"vcvarsall.simple":"C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\vcvarsall.bat",
+"vcvarsall":'"C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\vcvarsall.bat"',
+"java":"java",
+"saxon":"saxon9he.jar",
+"astyle":"astyle",
+"diff":"kdiff3",
+"cmake":"cmake",
+"msbuild":"msbuild",
+"make":"make",
+"xmllint":"xmllint",
+"graphviz":"dot",
+"doxygen":"doxygen"
+}
+
+def getCommand(key):
+	"""
+	Checks the command map, and returns the command specified by the key
+	"""
+	return commandMap[key]
