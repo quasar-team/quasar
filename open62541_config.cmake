fcdd4708141875b46de4e374c1620b737512dbaf
damiron <wardamo.zero@gmail.com>
2016-07-01 16:57:29 +0200

    Update open62541_config.cmake
    removed directory open62541, as it is not used anymore. The files that were previously on this directory are now inside open62541-compat.

--- open62541_config.cmake
+++ open62541_config.cmake
@@ -56,4 +56,4 @@ add_definitions(-Wall -Wno-deprecated -std=gnu++0x -Wno-literal-suffix)
 # 62xxx no uatrace
 set (LOGIT_HAS_UATRACE FALSE)
 
-set(BACKEND_SERVER_MODULES open62541 open62541-compat)
+set(BACKEND_SERVER_MODULES open62541-compat)
