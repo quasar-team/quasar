6cf36ca78e806f907ca9f7648a02f0f9bdfebb4e
Piotr Nikiel <piotr@nikiel.info>
2017-07-19 17:06:25 +0200

    Further cosmetics

--- AddressSpace/include/ASDelegatingMethod.h
+++ AddressSpace/include/ASDelegatingMethod.h
@@ -1,8 +1,22 @@
-/*
- * ASDelegatingMethod.h
+/* © Copyright CERN, 2015.  All rights not expressly granted are reserved.
+ * ASDelegatingVariable.h
  *
  *  Created on: Dec 31, 2016
- *      Author: pnikiel
+ *      Author: Piotr Nikiel
+ *
+ *  This file is part of Quasar.
+ *
+ *  Quasar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public Licence as published by
+ *  the Free Software Foundation, either version 3 of the Licence.
+ *
+ *  Quasar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public Licence for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Quasar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef ADDRESSSPACE_INCLUDE_ASDELEGATINGMETHOD_H_
