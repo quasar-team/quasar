b63fbf18c42b85dcbb29a777a9cc8f6e7b1702c6
Ben Farnham <ben.farnham@cern.ch>
2018-11-13 16:09:15 +0100

    removed unnecessary lib additions

--- open62541_config.cmake
+++ open62541_config.cmake
@@ -66,8 +66,8 @@ message ("using BOOST_LIBS [${BOOST_LIBS}]")
 # No OPC-UA Toolkit: using Open62541-compat instead. It is referenced in BACKEND_MODULES below
 add_definitions( -DBACKEND_OPEN62541 )
 SET( OPCUA_TOOLKIT_PATH "" )
-SET( OPCUA_TOOLKIT_LIBS_RELEASE "${PROJECT_BINARY_DIR}/open62541-compat/open62541/libopen62541.a" -lrt -lpthread)
-SET( OPCUA_TOOLKIT_LIBS_DEBUG "${PROJECT_BINARY_DIR}/open62541-compat/open62541/libopen62541.a" -lrt -lpthread)
+SET( OPCUA_TOOLKIT_LIBS_RELEASE "${PROJECT_BINARY_DIR}/open62541-compat/open62541/libopen62541.a" )
+SET( OPCUA_TOOLKIT_LIBS_DEBUG "${PROJECT_BINARY_DIR}/open62541-compat/open62541/libopen62541.a" )
 include_directories( ${PROJECT_BINARY_DIR}/open62541-compat/open62541 )
 
 #-----
