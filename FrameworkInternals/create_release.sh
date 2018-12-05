9db3f335d2fbbfbbccb151abbdd6781e548c9b85
Piotr Nikiel <piotr@nikiel.info>
2018-12-05 14:03:19 +0100

    improved error handling of create_release script

--- FrameworkInternals/create_release.sh
+++ FrameworkInternals/create_release.sh
@@ -17,16 +17,16 @@ if [ "`git status --porcelain ../ |wc -l`" != "0" ]; then
 fi
 
 # after this put a version info into files
-echo $TAG > ../Design/quasarVersion.txt
-echo "#define QUASAR_VERSION_STR \"$TAG\"" > ../Server/include/QuasarVersion.h
+echo $TAG > ../Design/quasarVersion.txt || exit 1
+echo "#define QUASAR_VERSION_STR \"$TAG\"" > ../Server/include/QuasarVersion.h || exit 1
 
-python ../quasar.py create_release
+python ../quasar.py create_release || exit 1
 
 # after this files.txt and genericFrameworkVersion.txt has changed so should be recommited
-git commit -m "files.txt update in preparation for tag=$1" ../
+git commit -m "files.txt update in preparation for tag=$1" ../ || exit 1
 
-git push origin master
+git push origin master || exit 1
 
-git tag -a "v$1"
+git tag -a "v$1" || exit 1
 
-git push origin "v$1"
+git push origin "v$1" || exit 1
