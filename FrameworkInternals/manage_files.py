ecc7bb7134c2dffdf48fda30bed53ef3fe7a7bf0
Paris Moschovakos <paris.moschovakos@cern.ch>
2019-02-14 16:05:25 +0100

    Remove windows-only code

--- FrameworkInternals/manage_files.py
+++ FrameworkInternals/manage_files.py
@@ -494,20 +494,6 @@ def mfCheckSvnIgnore():
 def mfDesignVsDevice():
 	"""Checks if the device files are outdated (By comparing with design), and hence if they should be regenerated."""
 	design_vs_device(os.getcwd())
-	
-if os.name == "nt":
-    def symlink_ms(source, link_name):
-        import ctypes
-        csl = ctypes.windll.kernel32.CreateSymbolicLinkW
-        csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
-        csl.restype = ctypes.c_ubyte
-        flags = 1 if os.path.isdir(source) else 0
-        try:
-            if csl(link_name, source.replace('/', '\\'), flags) == 0:
-                raise ctypes.WinError()
-        except:
-            pass
-    os.symlink = symlink_ms
     
 def symlinkIfNotExists(src, dst):
 	try:
