c70817fd43330fd77535fc1ff3f1454c9010b916
Piotr Nikiel <piotr@nikiel.info>
2019-11-26 16:15:13 +0100

    opening the file as binary

--- FrameworkInternals/manage_files.py
+++ FrameworkInternals/manage_files.py
@@ -128,7 +128,7 @@ class File(dict):
     def compute_md5(file_name):
         # Piotr: used the nice recipe from https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
         hash_md5 = hashlib.md5()
-        with open(file_name, 'r') as f:
+        with open(file_name, 'rb') as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
         return hash_md5.hexdigest()
