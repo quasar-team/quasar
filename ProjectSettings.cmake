adeed737755dd99b7eee924f4ee0d3f8a903c80a
Stefan Schlenker <stefan.schlenker@cern.ch>
2020-03-06 17:24:19 +0100

    add hint about BOOST_ROOT

--- ProjectSettings.cmake
+++ ProjectSettings.cmake
@@ -12,6 +12,7 @@ set(SERVER_LINK_DIRECTORIES  )
 ##
 ## Add here any additional boost libraries needed with their canonical name
 ## examples: date_time atomic etc.
+## Note: boost paths are resolved either from $BOOST_ROOT if defined or system paths as fallback
 ##
 set(ADDITIONAL_BOOST_LIBS )
 
