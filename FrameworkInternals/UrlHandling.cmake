d6315efe5e80d6e956abb08b6c7f3723475d9051
Stefan Schlenker <stefan.schlenker@cern.ch>
2020-03-19 17:40:45 +0100

    generalize GITHUB_REPLACEMENT to GIT_SERVER_REPLACEMENT to use it also for non-github URLs

--- FrameworkInternals/UrlHandling.cmake
+++ FrameworkInternals/UrlHandling.cmake
@@ -1,10 +1,20 @@
 function ( process_git_url URL )
 
-    # replace github base URL if defined in project settings
-    if (DEFINED GITHUB_REPLACEMENT)
+    # replace git server base URL if defined in project settings
+    # example: setting GIT_SERVER_REPLACEMENT="ssh://gitlab.com:7999/"
+    # and calling process_git_url(URL) with URL=https://github.com/quasar-team/MyQuasarModule.git
+    # results in URL to be set to "ssh://gitlab.com:7999/quasar-team/MyQuasarModule.git"
+    #
+    if (DEFINED GIT_SERVER_REPLACEMENT)
       set( NEW_URL ${${URL}} )
-      string(REPLACE "https://github.com/" ${GITHUB_REPLACEMENT} NEW_URL ${NEW_URL})
-      message("process_git_url(): Replaced ${${URL}} with ${NEW_URL}")
+      set( MATCHED )
+      string(REGEX MATCH "(http|https|ssh):\\/\\/[-a-zA-Z0-9@:%._\\+~#=]+\\.[a-z]+(:[0-9]+)?\\/" MATCHED ${NEW_URL})
+      message("process_git_url(): Matched ${MATCHED} in ${${URL}}")
+      if ( MATCHED )
+        string(REPLACE ${MATCHED} ${GITSERVER_REPLACEMENT} NEW_URL ${NEW_URL})
+        message("process_git_url(): Replaced to ${NEW_URL}")
+      endif()
+
       set( ${URL} ${NEW_URL} PARENT_SCOPE )
     endif()
 
