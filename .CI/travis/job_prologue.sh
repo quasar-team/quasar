8487a72ebaf910b5f7b90866385116b2ba0fda17
Piotr Nikiel <piotr@nikiel.info>
2020-03-20 19:16:52 +0100

    forgotten -y ..

--- .CI/travis/job_prologue.sh
+++ .CI/travis/job_prologue.sh
@@ -1,2 +1,2 @@
-           apt install python-enum34 python-jinja2 python-colorama ;
+           apt install -y python-enum34 python-jinja2 python-colorama ;
            ln -s /usr/bin/cmake /usr/bin/cmake3 ;
