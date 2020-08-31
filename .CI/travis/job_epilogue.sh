59fb135c15da297aab3a8dca32056ff2e1da8faa
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-08-31 11:34:32 +0200

    fixed ./ in the invocation

--- .CI/travis/job_epilogue.sh
+++ .CI/travis/job_epilogue.sh
@@ -1,5 +1,5 @@
-           quasar.py enable_module open62541-compat ;
-           quasar.py set_build_config open62541_config.cmake ;
-           quasar.py prepare_build Release ;
-           quasar.py build Release ;
+           ./quasar.py enable_module open62541-compat ;
+           ./quasar.py set_build_config open62541_config.cmake ;
+           ./quasar.py prepare_build Release ;
+           ./quasar.py build Release ;
            exit ;
