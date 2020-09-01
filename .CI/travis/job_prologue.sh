0628a8212ed2963dc93286431f73426139d9b1a8
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-09-01 10:41:37 +0200

    lxml installation in python3

--- .CI/travis/job_prologue.sh
+++ .CI/travis/job_prologue.sh
@@ -1,2 +1,2 @@
-           apt install -y python3-jinja2 python3-colorama ;
+           apt install -y python3-jinja2 python3-colorama python3-lxml ;
            ln -s /usr/bin/cmake /usr/bin/cmake3 ;
