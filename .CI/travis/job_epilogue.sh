1411cb2392a3e6bfb767de099209734086c9dae1
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-10-20 16:08:55 +0200

    Fix for array configentries and using open62541-compat branch wiht support for properties

--- .CI/travis/job_epilogue.sh
+++ .CI/travis/job_epilogue.sh
@@ -1,4 +1,4 @@
-           ./quasar.py enable_module open62541-compat ;
+           ./quasar.py enable_module open62541-compat OPCUA-2017_UaPropertyCache ;
            ./quasar.py set_build_config open62541_config.cmake ;
            ./quasar.py prepare_build Release ;
            ./quasar.py build Release ;
