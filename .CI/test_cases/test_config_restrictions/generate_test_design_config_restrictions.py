2036bdb0ffacb912a4a8793d61dd1c137ffe6317
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-11-10 10:04:50 +0100

    now all config entries have restrictions

--- .CI/test_cases/test_config_restrictions/generate_test_design_config_restrictions.py
+++ .CI/test_cases/test_config_restrictions/generate_test_design_config_restrictions.py
@@ -1,4 +1,35 @@
-# TODO: file header.
+#!/usr/bin/env python3
+# encoding: utf-8
+'''
+generate_test_design_config_restrictions.py
+
+@author:     Piotr Nikiel <piotr@nikiel.info>
+
+@copyright:  2020 CERN
+
+@license:
+Copyright (c) 2020, CERN.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+   and the following disclaimer in the documentation and/or other materials provided with the
+   distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS  OR
+IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED WARRANTIES  OF  MERCHANTABILITY
+AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  SPECIAL, EXEMPLARY, OR  CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT  NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS OR  SERVICES; LOSS OF
+USE, DATA, OR PROFITS; OR BUSINESS  INTERRUPTION) HOWEVER CAUSED AND ON ANY  THEORY  OF  LIABILITY,
+WHETHER IN  CONTRACT, STRICT  LIABILITY,  OR  TORT (INCLUDING  NEGLIGENCE OR OTHERWISE)  ARISING IN
+ANY WAY OUT OF  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+@contact:    quasar-developers@cern.ch
+'''
 
 import sys
 
@@ -43,6 +74,14 @@ def generate():
             if dataType in oracle.IntegerDataTypesRange:
                 range_tuple = oracle.IntegerDataTypesRange[dataType]
                 output('<d:restrictionByBounds minInclusive="{0}" maxInclusive="{1}"/>'.format(range_tuple[0]+1, range_tuple[1]-1))
+            elif dataType in ['OpcUa_Double', 'OpcUa_Float']:
+                output('<d:restrictionByBounds minInclusive="-12345678.999" maxInclusive="1234567890.999"/>')
+            elif dataType in ['UaString']:
+                output('<d:restrictionByEnumeration>')
+                output('<d:enumerationValue value="Parting is such sweet sorrow"/>')
+                output('<d:enumerationValue value="That I shall say good night till it be morrow."/>')
+                output('</d:restrictionByEnumeration>')
+            output('<d:restrictionByPattern pattern=".*"/>')
             output('</d:configRestriction>')
             output('</d:configentry>')
 
