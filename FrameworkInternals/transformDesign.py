affc82cb3f5ba152381a95e1743dcc9a8b88b458
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-02-15 15:59:43 +0100

    Now should solve astyle version check

--- FrameworkInternals/transformDesign.py
+++ FrameworkInternals/transformDesign.py
@@ -108,8 +108,12 @@ def run_indent_tool(unindented_content, fout):
        fout - a file object to which the output will be written"""
     try:
         astyle_version_check_process = subprocess.run(['astyle', '--version'],
-                                                    stderr=subprocess.PIPE, check=True)
-        astyle_version_text = astyle_version_check_process.stderr.decode('utf-8')
+                                                          stderr=subprocess.PIPE,
+                                                          stdout=subprocess.PIPE,
+                                                          check=True)
+        # note to line below: whether version is printed in stdout or stderr unfortunately depends
+        # on astyle version ... ironic ;-)
+        astyle_version_text = astyle_version_check_process.stderr.decode('utf-8') + astyle_version_check_process.stdout.decode('utf-8')
         if 'Artistic Style Version 3' not in astyle_version_text:
             raise Exception(('Found astyle with incompatible version. Astyle versions prior to 3 '
                              'are known to have a bug which makes them impossible to use with '
