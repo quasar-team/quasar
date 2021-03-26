8f95b3ea8bb966146b406ac5ee18f6bfeca1edcf
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-03-26 18:44:46 +0100

    intentionally committed broken meta

--- Meta/src/ASBuildInformation.cpp
+++ Meta/src/ASBuildInformation.cpp
@@ -44,7 +44,7 @@ ASBuildInformation::ASBuildInformation (
 	const std::string& host, const std::string& timestamp, const std::string commitID, const std::string& toolkitLibs):
     OpcUa::BaseObjectType (
         /*nodeId*/
-        nm->makeChildNodeId(parentNodeId,"BuildInformation"), "BuildInformation", nm->getNameSpaceIndex(), nm),
+        nm->makeChildNodeId(parentNodeId,"BuildInformation22"), "BuildInformation22", nm->getNameSpaceIndex(), nm),
     m_typeNodeId (typeNodeId)
 
 
