85fb5b40a04948d5145c8d6297246fb3f28dbebc
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-04-16 12:20:18 +0200

    Using 1.3.8-rc0 as the baseline for CI tests

--- .CI/travis/job_epilogue.sh
+++ .CI/travis/job_epilogue.sh
@@ -1,4 +1,4 @@
-           ./quasar.py enable_module open62541-compat v1.3.7-rc0 ;
+           ./quasar.py enable_module open62541-compat v1.3.8-rc0 ;
            ./quasar.py set_build_config open62541_config.cmake ;
            ./quasar.py prepare_build Release ;
            ./quasar.py build Release ;
