4307d88338699b38127a824afa6567ec220fe979
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-06-30 17:01:30 +0200

    Trying to also run w/ OPCUA-2363

--- .CI/travis/job_epilogue.sh
+++ .CI/travis/job_epilogue.sh
@@ -1,4 +1,4 @@
-           ./quasar.py enable_module open62541-compat OPCUA-2364_open62541_v1.2.2  ;
+           ./quasar.py enable_module open62541-compat OPCUA-2363_profit_from_uaprint  ;
            ./quasar.py set_build_config open62541_config.cmake ;
            ./quasar.py prepare_build Release ;
            ./quasar.py build Release ;
