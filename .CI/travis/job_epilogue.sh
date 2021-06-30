0cbb1a044acbb98703e4bfbce6f9d7aad9b9b517
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-06-30 10:01:58 +0200

    Trying open62541 1.2.2

--- .CI/travis/job_epilogue.sh
+++ .CI/travis/job_epilogue.sh
@@ -1,4 +1,4 @@
-           ./quasar.py enable_module open62541-compat v1.3.9-rc0  ;
+           ./quasar.py enable_module open62541-compat OPCUA-2364_open62541_v1.2.2  ;
            ./quasar.py set_build_config open62541_config.cmake ;
            ./quasar.py prepare_build Release ;
            ./quasar.py build Release ;
