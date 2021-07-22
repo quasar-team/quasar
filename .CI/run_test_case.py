a1ae45491614167a1ffec4d1a63bc791635737d1
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-07-22 11:04:50 +0200

    backend setup

--- .CI/run_test_case.py
+++ .CI/run_test_case.py
@@ -7,17 +7,25 @@ import argparse
 def clone_quasar(test_branch):
     os.system(f'git clone --recursive -b {test_branch} --depth=1 https://github.com/quasar-team/quasar.git')
 
+def prepare_opcua_backend(opcua_backend, open62541_compat_branch):
+    if opcua_backend == 'o6':
+        os.system(f'./quasar.py enable_module open62541-compat {open62541_compat_branch}')
+    pass
+
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--clone', action='store_true')
     parser.add_argument('--quasar_branch', default='master')
-
+    parser.add_argument('--opcua_backend')
+    parser.add_argument('--open62541_compat_branch')
 
     args = parser.parse_args()
 
     if args.clone:
         clone_quasar(args.quasar_branch)
+
+    prepare_opcua_backend(args.opcua_backend, args.open62541_compat_branch)
 
     print('a')
     pass
