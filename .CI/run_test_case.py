d06b29d947e0aecc3ea8302a1c7a86acf1fec378
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-07-23 10:26:44 +0200

    converging towards UASDK CI

--- .CI/run_test_case.py
+++ .CI/run_test_case.py
@@ -20,8 +20,10 @@ def prepare_opcua_backend(opcua_backend, open62541_compat_branch):
     if opcua_backend == 'o6':
         os.system(f'./quasar.py enable_module open62541-compat {open62541_compat_branch}')
         os.system(f'./quasar.py set_build_config open62541_config.cmake')
+    elif opcua_backend == 'uasdk':
+        invoke_and_check('./quasar.py set_build_config .CI/travis/build_configs/uasdk-eval.cmake')
     else:
-        raise Exception("backend unsupported")
+        raise Exception(f"OPCUA backend {opcua_backend} was not recognized")
 
 def build():
     invoke_and_check('./quasar.py build Release')
