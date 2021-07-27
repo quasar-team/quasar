c39a22c7b1457930989de7e388f17d8c4e1762ad
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-07-27 11:52:44 +0200

    job_epilogue no longer used

--- .CI/travis/job_epilogue.sh
+++
@@ -1,5 +0,0 @@
-           ./quasar.py enable_module open62541-compat OPCUA-2419_add_property_node_ref_not_setting_datatype_correctly ;
-           ./quasar.py set_build_config open62541_config.cmake ;
-           ./quasar.py prepare_build Release ;
-           ./quasar.py build Release ;
-           ./.CI/travis/server_fixture.py --command_to_run uasak_dump ;
