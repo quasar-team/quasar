5f3d05834ce8ce14b7822c8967863247ceb6e9d2
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-11-30 18:20:00 +0100

    Add --format for the xmllint options and also improved the file to be more PIP8 compliant

--- FrameworkInternals/configurationGenerators.py
+++ FrameworkInternals/configurationGenerators.py
@@ -11,10 +11,23 @@ configurationGenerators.py
 @license:
 Copyright (c) 2015, CERN, Universidad de Oviedo.
 All rights reserved.
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+   and the following disclaimer in the documentation and/or other materials provided with the
+   distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS  OR
+IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED WARRANTIES  OF  MERCHANTABILITY
+AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  SPECIAL, EXEMPLARY, OR  CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT  NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS OR  SERVICES; LOSS OF
+USE, DATA, OR PROFITS; OR BUSINESS  INTERRUPTION) HOWEVER CAUSED AND ON ANY  THEORY  OF  LIABILITY,
+WHETHER IN  CONTRACT, STRICT  LIABILITY,  OR  TORT (INCLUDING  NEGLIGENCE OR OTHERWISE)  ARISING IN
+ANY WAY OUT OF  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 @contact:    quasar-developers@cern.ch
 '''
@@ -25,17 +38,19 @@ from externalToolCheck import subprocessWithImprovedErrorsPipeOutputToFile
 from commandMap import getCommand
 
 def generateConfiguration(context):
-    """Generates the file Configuration.xsd. This method is called automatically by cmake, it does not need to be called by the user."""
-    config_xsd_path = os.path.join(context['projectBinaryDir'],'Configuration','Configuration.xsd')
+    """Generates the file Configuration.xsd."""
+    config_xsd_path = os.path.join(context['projectBinaryDir'], 'Configuration', 'Configuration.xsd')
     try:
-        transformByKey( TransformKeys.CONFIGURATION_XSD, {
-                'context':context,
-                'metaXsdPath':os.path.join(context['projectSourceDir'],'Meta','config','Meta.xsd').replace('\\','/') } )
+        transformByKey(TransformKeys.CONFIGURATION_XSD, {
+            'context':context,
+            'metaXsdPath':os.path.join(context['projectSourceDir'], 'Meta', 'config', 'Meta.xsd')
+                          .replace('\\', '/')})
     except:
         if os.path.isfile(config_xsd_path):
             os.remove(config_xsd_path)
         raise
     subprocessWithImprovedErrorsPipeOutputToFile(
-            [getCommand("xmllint"), "--xinclude", getTransformOutput(TransformKeys.CONFIGURATION_XSD, {'context':context})],
-            config_xsd_path,
-            getCommand("xmllint"))
+        [getCommand("xmllint"), "--format", "--xinclude",
+         getTransformOutput(TransformKeys.CONFIGURATION_XSD, {'context':context})],
+        config_xsd_path,
+        getCommand("xmllint"))
