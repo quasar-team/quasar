b39e6d51c9671c2bfb45e10bd9e52653bbf0c761
Paris Moschovakos <paris.moschovakos@cern.ch>
2022-09-30 11:39:44 +0200

    Script to update the versions

---
+++ Documentation/tools/update_versions.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import json
+
+PMOSCHOV_PASS = os.environ.get('PMOSCHOV_PASS')
+
+JSON_FILE = './Documentation/source/_static/versions.json'
+JSON_FILE_LOCAL = '/home/pmoschov/quasar/versions.json'
+
+VERSIONS_FOLDER = '/usr/share/nginx/quasar/version'
+EOS_VERSIONS_FOLDER = '/eos/project-q/quasar/www/version'
+
+if __name__ == '__main__':
+  cp_local_command = f'cp {JSON_FILE} {JSON_FILE_LOCAL}'
+  print(f'Copying last version.json to local folder: ', cp_local_command)
+
+  for folder_version in os.listdir(VERSIONS_FOLDER):
+    print('Processing version: ', folder_version)
+    
+    cp_nginx_command = f'echo \"{PMOSCHOV_PASS}\" | sudo -S cp {JSON_FILE_LOCAL} {VERSIONS_FOLDER}/{folder_version}/_static/versions.json'
+    os.system(cp_nginx_command)
+    
+    cp_eos_command = f'cp {JSON_FILE_LOCAL} {EOS_VERSIONS_FOLDER}/{folder_version}/_static/versions.json'
+    os.system(cp_eos_command)
+
+  exit(0)
