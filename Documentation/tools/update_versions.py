e794f3a1059e41a9aa7e3a5fd0615eeed2093e5f
Paris Moschovakos <paris.moschovakos@cern.ch>
2022-12-13 16:15:47 +0100

    OPCUA-2919

--- Documentation/tools/update_versions.py
+++ Documentation/tools/update_versions.py
@@ -13,6 +13,7 @@ EOS_VERSIONS_FOLDER = '/eos/project-q/quasar/www/version'
 if __name__ == '__main__':
   cp_local_command = f'cp {JSON_FILE} {JSON_FILE_LOCAL}'
   print(f'Copying last version.json to local folder: ', cp_local_command)
+  os.system(cp_local_command)
 
   for folder_version in os.listdir(VERSIONS_FOLDER):
     print('Processing version: ', folder_version)
