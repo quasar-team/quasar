6a3e2763241f930f50486670171ca05eb5463e20
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-04-14 13:33:14 +0200

    Restore all set_build_config and append found XercesC and LibXml2, even if XML_LIBS is defined

--- .CI/run_test_case.py
+++ .CI/run_test_case.py
@@ -20,8 +20,9 @@ def clone_quasar(test_branch):
 def prepare_opcua_backend(opcua_backend, open62541_compat_branch):
     if opcua_backend == 'o6':
         invoke_and_check(f'./quasar.py enable_module open62541-compat {open62541_compat_branch}')
+        invoke_and_check(f'./quasar.py set_build_config open62541_config.cmake')
     elif opcua_backend == 'uasdk':
-        pass
+        invoke_and_check('./quasar.py set_build_config .CI/travis/build_configs/uasdk-eval.cmake')
     else:
         raise Exception(f"OPCUA backend {opcua_backend} was not recognized")
 
